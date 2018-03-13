ff57e30fd7b6a5b60fcbd2eaba7883d9210ebfbf
Joshua Baubry <joshua.baubry@gmail.com>
2018-03-13 18:57:03 +0100

    Ajout de la ma vue 2

--- app/src/main/java/fr/dronemarin/MainActivity.java
+++ app/src/main/java/fr/dronemarin/MainActivity.java
@@ -1,7 +1,11 @@
 package fr.dronemarin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import fr.dronemarin.controleur.Vue2Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(MainActivity.this,WaypointActivity.class);
+               startActivityForResult(intent,1);
+           }
+       });
     }
 }
