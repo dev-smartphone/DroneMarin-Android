158dc80a42c38a2f163265e1d20948aa4a015830
Joshua Baubry <joshua.baubry@gmail.com>
2018-03-26 08:38:23 +0200

    Optimisation des imports, suppression de code inutile

--- app/src/main/java/fr/dronemarin/controleur/WaypointDialog.java
+++ app/src/main/java/fr/dronemarin/controleur/WaypointDialog.java
@@ -1,5 +1,4 @@
 package fr.dronemarin.controleur;
-
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -12,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-
 import fr.dronemarin.R;
 import fr.dronemarin.modele.Modele;
 import fr.dronemarin.modele.Waypoint;
